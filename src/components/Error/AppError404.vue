2a3ff0330f2418001ddb5b1c1b2282b9f06be9cb
RabieImghi <rabieaitimghi7@gmail.com>
2024-04-21 23:19:28 +0100

    Update error page layout and content in AppError404.vue

--- src/components/Error/AppError404.vue
+++ src/components/Error/AppError404.vue
@@ -1,45 +1,59 @@
 <template>
-    <div class="error-page">
-        <img src="https://www.google.com/images/errors/robot.png" alt="Error 404" class="error-image">
-        <h1 class="error-title">404</h1>
-        <p class="error-message">Page not found</p>
-    </div>
+    <section class="p-0 bg-img cover-background" style="background-image: url(https://bootdey.com/img/Content/bg1.jpg);">
+        <div class="container-fluid d-flex flex-column">
+            <div class="row align-items-center justify-content-center min-vh-100">
+                <div class="col-md-9 col-lg-6 my-5">
+                    <div class="text-center error-page">
+                        <h1 class="mb-0 text-secondary">404</h1>
+                        <h2 class="mb-4 text-white">Sorry, something went wrong!</h2>
+                        <p class="w-sm-80 mx-auto mb-4 text-white">This page is incidentally inaccessible because of support. We will back very before long much obliged for your understanding</p>
+                        <div>
+                            <router-link to="/user/" class="btn btn-info btn-lg me-sm-2 mb-2 mb-sm-0">Return Home</router-link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <style scoped>
+body{margin-top:20px;}
+.cover-background {
+    position: relative !important;
+    background-size: cover !important;
+    overflow: hidden !important;
+    background-position: center !important;
+    background-repeat: no-repeat !important;
+}
+.p-0 {
+    padding: 0!important;
+}
+section {
+    padding: 120px 0;
+    overflow: hidden;
+    background: #fff;
+}
+
 .error-page {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-    background-color: #f8f9fa;
+    background-color: #BABABA4A;
+    -webkit-backdrop-filter: blur(9px);
+    backdrop-filter: blur(15px);
+    border: 1px solid rgba(234,234,235,0.2);
+    padding: 80px 20px;
 }
-
-.error-image {
-    width: 200px;
-    height: 200px;
-    animation: rotate 4s infinite linear;
+.text-center {
+    text-align: center!important;
 }
-
-.error-title {
-    font-size: 48px;
-    color: #dc3545;
-    margin-top: 20px;
+.error-page h1 {
+    font-size: 200px;
+    line-height: 1;
+    font-weight: 600;
 }
-
-.error-message {
-    font-size: 24px;
-    color: #343a40;
-    margin-top: 10px;
+.text-secondary {
+    color: #15395A !important;
 }
-
-@keyframes rotate {
-    0% {
-        transform: rotate(0deg);
-    }
-    100% {
-        transform: rotate(360deg);
-    }
+.mb-4 {
+    margin-bottom: 1.5rem!important;
 }
 </style>
