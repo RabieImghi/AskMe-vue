6b3ee87409b023847b1cd5567219c19973cab979
RabieImghi <rabieaitimghi7@gmail.com>
2024-04-21 22:08:58 +0100

    Refactor user navigation links in AppLayout.vue and update TinyMCE script source URL in index.html

--- src/components/User/AppLayout.vue
+++ src/components/User/AppLayout.vue
@@ -91,7 +91,7 @@
                         <span class="px-3">Logout</span> 
                     </a>
                     <hr class="m-none nav-mobile">
-                    <router-link class="nav-mobile d-none nav-link p-2 d-flex gap-2 align-items-center" to="/user/AskQuesion">
+                    <router-link v-if="tokenExists" class="nav-mobile d-none nav-link p-2 d-flex gap-2 align-items-center" to="/user/AskQuesion">
                         <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
                             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
                             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
